6b45fd1d8fc183b3cce802766f4e820d4a89e9a6
Daniela Ramos <danirc01@gmail.com>
2023-10-26 15:30:42 -0500

    ejercicio de intro completado

--- IntroJavaScript/main.js
+++ IntroJavaScript/main.js
@@ -1,30 +1,38 @@
-let miNombre = "Daniela"
+//1
+const miNombre = "Daniela"
 console.log (miNombre)
 
-let miApellido = "Ramos"
+//2
+const miApellido = "Ramos"
 console.log(miApellido)
 
-let miEdad = 24
+//3
+const miEdad = 24
 console.log(miEdad)
 
-let miMascota = "Chopin"
+//4
+const miMascota = "Chopin"
 console.log(miMascota)
 
+//5
 let edadMascota = 6
 console.log(edadMascota)
 
-let nombreCompleto = miNombre + " " + miApellido
+//7
+const nombreCompleto = miNombre + " " + miApellido
 
+//8
 let textoPresentacion = `Hola! mi nombre es ${nombreCompleto}, tengo ${miEdad} años. Mi mascota se llama ${miMascota} y tiene ${edadMascota} años.`
 console.log(textoPresentacion)
 
 
-
+//9
 let sumaEdades = miEdad+edadMascota
 let restaEdades = miEdad-edadMascota
 let productoEdades = miEdad*edadMascota
 let divisionEdades = miEdad/edadMascota
 
+//10
 let alumno = {
     nombre: "Carlos",
     apellido: "Rodriguez",
@@ -41,6 +49,8 @@ console.log(alumno.edad)
 console.log(alumno.nacionalidad)
 console.log(alumno.colorFavorito)
 
+
+//11
 let mascota = {
     nombreMascota: "Chopin",
     tipoMascota: "gato",
@@ -56,6 +66,7 @@ console.log(mascota.sexoMascota)
 console.log(mascota.actividadFavorita)
 
 
+//12
 const frutas = [ "Mango","Kiwi","Limón","Manzana","Banano" ]
 console.log( frutas )
 console.log( frutas[0] )
@@ -65,11 +76,13 @@ console.log( frutas[3] )
 console.log( frutas[4] )
 
 
+//13
 let mayorDeEdad = prompt("Ingresa tu edad")
 let soyMayorDeEdad = mayorDeEdad >= 18
 console.log(soyMayorDeEdad)
 
 
+//14
 const numeros = [ 5,13,20,48,30 ]
 console.log(numeros)
 console.log( numeros[0] )
@@ -79,6 +92,7 @@ console.log( numeros[3] )
 console.log( numeros[4] )
 
 
+//15
 const familia = [ 
     {miembro: "mamá"},
     {miembro: "tío"},
@@ -94,10 +108,12 @@ console.log(familia[3])
 console.log(familia[4])
 
 
+//16
 let textoAleatorio = `Mi ${familia[4].miembro} compró ${numeros[3]} kilos de ${frutas[1]}`
 console.log(textoAleatorio)
 
 
+//17
 let edad1, edad2
 edad1 = prompt("ingresa tu edad")
 edad2 = prompt("ingresa la edad de tu compañero")
@@ -107,3 +123,29 @@ let soyMenor = "Mi edad es menor a la de mi compañero: " + ( edad1 < edad2 )
 console.log(edadesIguales)
 console.log(soyMayor)
 console.log(soyMenor)
+
+
+//18
+let primeraEdad, altura
+primeraEdad = Number(prompt("ingresa tu edad"))
+altura = Number(prompt("introduce tu altura en cm"))
+let puedeSubir = "Puede subir a la atracción "
+if(primeraEdad>6 && altura>=120){
+    alert(puedeSubir + "(true)")
+}
+else{
+    alert(puedeSubir + "(false)")
+}
+
+
+//19
+let tipoDePase, saldo
+tipoDePase = prompt("qué tipo de pase tienes?")
+saldo = Number(prompt("introduce tu saldo"))
+const puedePasar = "La persona puede pasar "
+if(tipoDePase === "vip" && saldo>1000){
+    console.log(puedePasar + "(true)")
+}
+else{
+    console.log(puedePasar + "(false)")
+}
